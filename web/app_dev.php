600e6b41b9fd4dcef1e099265d390024748b040d
weeger <romain.weeger@gmail.com>
2016-11-03 16:27:23 +0100

    Add IP access for custom dev envs.

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,22 +9,24 @@ use Symfony\Component\HttpFoundation\Request;
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array(
-        '127.0.0.1',
-        '::1',
-    ))
+  || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+  || !in_array(@$_SERVER['REMOTE_ADDR'], array(
+    '127.0.0.1',
+    '::1',
+    '83.156.34.69', // RW dev server
+    '51.255.174.234', // MC dev server
+  ))
 ) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+  header('HTTP/1.0 403 Forbidden');
+  exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
 }
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
+$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
+require_once __DIR__ . '/../app/AppKernel.php';
 
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('dev', TRUE);
 $kernel->loadClassCache();
-$request = Request::createFromGlobals();
+$request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
