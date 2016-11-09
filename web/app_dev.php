bfe8dc0360a78bf9f8fd26f0737532f8018b512b
weeger <romain.weeger@gmail.com>
2016-11-09 15:29:09 +0100

    IP restriction enabled

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,21 +8,17 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-
-/*
 if (isset($_SERVER['HTTP_CLIENT_IP'])
   || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
   || !in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
-    '83.156.34.69', // RW dev server
-    '51.255.174.234', // MC dev server
+    '37.166.214.184', // AV
   ))
 ) {
   header('HTTP/1.0 403 Forbidden');
   exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
 }
-*/
 
 $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 require_once __DIR__ . '/../app/AppKernel.php';
