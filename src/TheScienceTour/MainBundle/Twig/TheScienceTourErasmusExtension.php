7c2ef7fa0f57134c123d3e16ddca51c19c9839d3
weeger <romain.weeger@gmail.com>
2016-11-09 18:50:22 +0100

    Set erasmus CSS globally

---
+++ src/TheScienceTour/MainBundle/Twig/TheScienceTourErasmusExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TheScienceTour\MainBundle\Twig;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class TheScienceTourErasmusExtension extends \Twig_Extension {
+  private $session;
+
+  function __construct(Session $session) {
+    $this->session = $session;
+  }
+
+  public function getFunctions() {
+    return array(
+      new \Twig_SimpleFunction('isErasmus', array($this, 'isErasmusFunction')),
+    );
+  }
+
+  public function getName() {
+    return 'erasmus';
+  }
+
+  public function isErasmusFunction() {
+    return $this->session->get('isErasmus', FALSE);
+  }
+}
