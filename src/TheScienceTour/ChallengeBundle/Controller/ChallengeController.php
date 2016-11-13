3e2e70993132a28f5cc65ef8ef5b6100b3516ad9
Michel Cadennes <michel.cadennes@sens-commun.fr>
2016-11-13 15:48:25 +0100

    Prise en compte du domaine pour l'affichage des défis
    1) LA variable de session 'isErasmus' est interrogée pour savoir quels sont les contenus qui doivent être affichés en mode "liste" (méthode 'challengesAction' > l'information est traitée par la requête (Repository)
    
    A TESTER
    TODO : Valier la stratégie de sélection des contenus
    
    2) La méthode 'editChallengeAction' est modifiée de manière à pouvoir :
    -- spécifier qu'un défi est lié à Erasmus
    -- et que dans ce cas, il est dans une langue déterminée (par l'utilisateur)
    
    Par défaut, la langue sélectionnée pour le contenu sera la _locale passée dans l'URL
    
    A TESTER
    TODO :
    a) Extraire le formulaire du contrôleur
    b) Modifier la fonction pour permettre la gestion des traductions
    c) Ajouter un bouton 'Ajouter une traduction' dans la page de formulaire si le contenu est Erasmus (seulement ?)

--- src/TheScienceTour/ChallengeBundle/Controller/ChallengeController.php
+++ src/TheScienceTour/ChallengeBundle/Controller/ChallengeController.php
@@ -11,13 +11,13 @@ use TheScienceTour\MessageBundle\Document\Message;
 
 
 class ChallengeController extends Controller {
-	
-	
-	
+
+
+
 	/**
 	 * Render challenge panels
 	 */
-	
+
 	public function challengePanelsAction($challengeList, $nbByRow, $mgr) {
 		return $this->render('TheScienceTourChallengeBundle::challengePanels.html.twig', array(
 			'challengeList' => $challengeList,
@@ -25,58 +25,63 @@ class ChallengeController extends Controller {
 			'mgr' => $mgr
 		));
 	}
-	
-	
-	
+
+
+
 	/**
-	 * Show all challenges
+	 * challengesAction Show all challenges
+	 *
+	 * @return Response List of challenges (closed and in progress)
 	 */
-	
+
 	public function challengesAction() {
 		$dm = $this->get('doctrine_mongodb')->getManager();
+		$isErasmus = $this->get('session')->get('isErasmus');
+		// $locale = $this->getLocale();
+
 		$challengeRepo = $dm->getRepository('TheScienceTourChallengeBundle:Challenge');
 
-		$inProgressChallenges = $challengeRepo->findInProgress();
-		
-		$pastChallengesQuery = $challengeRepo->findPast();
+		$inProgressChallenges = $challengeRepo->findInProgress($isErasmus);
+
+		$pastChallengesQuery = $challengeRepo->findPast($isErasmus);
 		$paginator = $this->get('knp_paginator');
 		$pastChallenges = $paginator->paginate(
 			$pastChallengesQuery,
 			$this->get('request')->query->get('page', 1),
 			6
 		);
-		
+
 		$inProgressProjects =  new \Doctrine\Common\Collections\ArrayCollection();
 		foreach ($inProgressChallenges as $challenge) {
 			foreach ($challenge->getProjects() as $project) {
 				$inProgressProjects[] = $project;
-			} 
+			}
 		}
-		
+
 		return $this->render('TheScienceTourChallengeBundle::challenges.html.twig', array(
 			'inProgressChallenges' => $inProgressChallenges,
 			'pastChallenges' => $pastChallenges,
 			'inProgressProjects' => $inProgressProjects
 		));
-		
+
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Show one challenge
 	 */
-	
+
 	public function challengeAction($id, $tab) {
 		$user = $this->getUser();
-		
+
 		$challenge = $this->get('doctrine_mongodb')
 			->getRepository('TheScienceTourChallengeBundle:Challenge')
 			->find($id);
 		if (!$challenge) {
 			throw $this->createNotFoundException('Aucun défi trouvé avec l\'id '.$id);
 		}
-		
+
 		if ($user) {
 			$dm = $this->get('doctrine_mongodb')->getManager();
 			if ($tab == "chats") {
@@ -86,16 +91,16 @@ class ChallengeController extends Controller {
 			$dm->persist($user);
 			$dm->flush();
 		}
-		
+
 		$isEditable = $this->get('security.context')->isGranted('ROLE_PROJECT_MOD');
-		
+
 		$paginator = $this->get('knp_paginator');
 		$projects = $paginator->paginate(
 				$challenge->getProjects(),
 				$this->get('request')->query->get('page', 1),
 				9
 		);
-		
+
 		return $this->render('TheScienceTourChallengeBundle::challenge.html.twig', array(
 			'challenge' => $challenge,
 			'projects' => $projects,
@@ -103,14 +108,21 @@ class ChallengeController extends Controller {
 			'tab' => $tab,
 		));
 	}
-	
-	
-	
+
+
+
 	/**
-	 * Add or edit a challenge
+	 * editChallengeAction Add or edit a challenge
+	 *
+	 * @var integer $id Id of the document to edit (NULL if creation)
+	 * @var integer $translated_id Id of the document to be translated (NULL by default)
+	 * @return Response The form to be displayed on screen
 	 */
-	
+
 	public function editChallengeAction($id) {
+		$isErasmus = $this->get('session')->get('isErasmus');
+		// $locale = $this->getLocale();
+
 		$newChallenge = ($id == null);
 		$user = $this->getUser();
 		if (!$user || !$this->get('security.context')->isGranted('ROLE_SUPER_ANIM')) {
@@ -120,6 +132,11 @@ class ChallengeController extends Controller {
 			$challenge = new Challenge();
 			$challenge->setCreator($user);
 			$challenge->setStartedAt(new \DateTime("tomorrow"));
+			$challenge->setIsErasmus($isErasmus);
+			// TODO : Confirmer le choix de la langue pour le Science Tour
+			if (!isErasmus) {
+				$challenge->setLanguage($this->getLocale());
+			}
 		} else {
 			$challenge = $this->get('doctrine_mongodb')
 				->getRepository('TheScienceTourChallengeBundle:Challenge')
@@ -131,16 +148,23 @@ class ChallengeController extends Controller {
 		$form = $this->createFormBuilder($challenge, array('cascade_validation' => true))
 			->add('title', 'text')
 			->add('startedAt', 'date')
-			->add('duration', 'integer', array('attr' => array('min' => 1)))
-			->add('durationUnit', 'choice', array(
-				'choices'   => array('day' => 'Jours', 'week' => 'Semaines', 'month' => "Mois")
-			))
+			->add('duration', 'integer', ['attr' => ['min' => 1]])
+			->add('durationUnit', 'choice', [
+				'choices' => ['day' => 'Jours', 'week' => 'Semaines', 'month' => "Mois"]
+			])
 			->add('description', 'purified_textarea')
 			->add('rules', 'purified_textarea')
 			->add('tools', 'collection', array('type' => new ChallengeResType(), 'allow_add' => true, 'allow_delete' => true, 'by_reference' => false))
 			->add('materials', 'collection', array('type' => new ChallengeResType(), 'allow_add' => true, 'allow_delete' => true, 'by_reference' => false))
 			->add('premises', 'collection', array('type' => new ChallengeResType(), 'allow_add' => true, 'allow_delete' => true, 'by_reference' => false))
 			->add('skills', 'collection', array('type' => new ChallengeResType(), 'allow_add' => true, 'allow_delete' => true, 'by_reference' => false));
+		if ($isErasmus) {
+			$form->add('language', 'choice',
+						['choices' => $this->container->getParameters('erasmusLanguages'),
+						 'preferres_choices' => $this->getLocale(),
+						 'multiple' => false,
+						 'expanded' => false]);
+		}
 		if ($newChallenge) {
 			$form->add('picture', 'sonata_media_type', array(
 					'provider' => 'sonata.media.provider.image',
@@ -154,7 +178,7 @@ class ChallengeController extends Controller {
 			));
 		}
 		$form = $form->getForm();
-	
+
 		$request = $this->get('request');
 		if ($request->getMethod() == 'POST') {
 			$challenge->removeRes();
@@ -170,27 +194,27 @@ class ChallengeController extends Controller {
 				return $this->redirect($this->generateUrl('tst_challenge', array('id' => $challenge->getId())));
 			}
 		}
-	
+
 		return $this->render('TheScienceTourChallengeBundle::edit.html.twig', array(
 				'form' => $form->createView(),
 				'challenge' => $challenge,
 				'newChallenge' => $newChallenge
 		));
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Subscribe to a challenge
 	 */
-	
+
 	public function subscribeAction($id) {
 
 		$user = $this->getUser();
 		if (!$user) {
 			throw new AccessDeniedException();
 		}
-	
+
 		$challenge = $this->get('doctrine_mongodb')
 			->getRepository('TheScienceTourChallengeBundle:Challenge')
 			->find($id);
@@ -200,28 +224,28 @@ class ChallengeController extends Controller {
 		if ($challenge->getSubscribers()->contains($user)) {
 			throw $this->createNotFoundException('Vous êtes déjà abonné à ce défi !');
 		}
-	
+
 		$dm = $this->get('doctrine_mongodb')->getManager();
 		$challenge->addSubscriber($user);
 		$dm->persist($challenge);
 		$dm->flush();
-	
+
 		return $this->redirect($this->generateUrl('tst_challenge', array('id' => $id)));
-		
+
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Unsubscribe to a challenge
 	 */
-	
+
 	public function unsubscribeAction($id) {
 		$user = $this->getUser();
 		if (!$user) {
 			throw new AccessDeniedException();
 		}
-	
+
 		$challenge = $this->get('doctrine_mongodb')
 			->getRepository('TheScienceTourChallengeBundle:Challenge')
 			->find($id);
@@ -231,21 +255,21 @@ class ChallengeController extends Controller {
 		if (!$challenge->getSubscribers()->contains($user)) {
 			throw $this->createNotFoundException('Vous n\'êtes pas abonné à ce défi !');
 		}
-	
+
 		$dm = $this->get('doctrine_mongodb')->getManager();
 		$challenge->removeSubscriber($user);
 		$dm->persist($challenge);
 		$dm->flush();
-	
+
 		return $this->redirect($this->generateUrl('tst_challenge', array('id' => $id)));
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Add chat/message
 	 */
-	
+
 	public function addChatMessageAction($id, $idchat) {
 		$newChat = ($idchat == null);
 		$user = $this->getUser();
@@ -255,11 +279,11 @@ class ChallengeController extends Controller {
 		$challenge = $this->get('doctrine_mongodb')
 			->getRepository('TheScienceTourChallengeBundle:Challenge')
 			->find($id);
-		
+
 		if (!$challenge) {
 			throw $this->createNotFoundException('Aucun défi trouvé avec l\'id '.$id);
 		}
-		
+
 		if ($newChat) {
 			$chat = new Chat();
 		} else {
@@ -273,37 +297,37 @@ class ChallengeController extends Controller {
 				throw $this->createNotFoundException('Cette discussion ne fait pas partie du défi');
 			}
 		}
-		
+
 		$error_message = null;
 		$mess = "";
-		
+
 		$request = $this->get('request');
 		if ($request->getMethod() == 'POST') {
 			$mess = $request->request->get('message');
-			
+
 			$html_purifier = $this->container->get('exercise_html_purifier.twig_extension');
 			$mess_nohtml = $html_purifier->purify($mess, 'no_html');
-						
+
 			if ($mess == null || $mess_nohtml == "") {
 				$error_message = "Tu dois entrer un message !";
 			} elseif (strlen($mess_nohtml) > 5000) {
 				$error_message = "Tu es trop bavard ! (5000 caractères maximum)";
 			} else {
 				$dm = $this->get('doctrine_mongodb')->getManager();
-				
+
 				$chat->addUser($user);
-					
+
 				$message = new Message();
 				$message->setAuthor($user);
 				$message->setContent($mess);
-				
+
 				$chat->addMessage($message);
-				
+
 				if ($newChat) {
 					$mess_begin = strlen($mess_nohtml) > 25 ? mb_substr($mess_nohtml, 0, 25, 'utf-8').'...' : $mess_nohtml;
 					$chat->setTitle($challenge->getTitle().' - '.$mess_begin);
 					$challenge->addChat($chat);
-					
+
 					if ($user != $challenge->getCreator()) {
 						$from = $this->container->getParameter('mailer_sender_address');
 						$mail = \Swift_Message::newInstance()
@@ -315,7 +339,7 @@ class ChallengeController extends Controller {
 								'chat' => $chat,
 								'author' => $user
 						)));
-							
+
 						$this->get('mailer')->send($mail);
 						$challenge->getCreator()->addNotification("challenge-chats", $challenge->getId());
 						$dm->persist($challenge->getCreator());
@@ -341,18 +365,18 @@ class ChallengeController extends Controller {
 							}
 						}
 						$this->get('mailer')->send($mail);
-					}	
+					}
 				}
 
 				$challenge->setUpdatedAt(new \Datetime);
-				
+
 				$dm->persist($chat);
-				$dm->persist($challenge);	
+				$dm->persist($challenge);
 				$dm->flush();
-				
+
 				return $this->redirect($this->generateUrl('tst_challenge', array('id' => $id, 'tab' => "chats")));
 			}
-			
+
 		}
 
 		return $this->render('TheScienceTourChallengeBundle:chat:addChatMessage.html.twig', array(
@@ -364,9 +388,9 @@ class ChallengeController extends Controller {
 		));
 
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Delete chat
 	 */
@@ -379,35 +403,35 @@ class ChallengeController extends Controller {
 		$challenge = $this->get('doctrine_mongodb')
 			->getRepository('TheScienceTourChallengeBundle:Challenge')
 			->find($id);
-	
+
 		if (!$challenge) {
 			throw $this->createNotFoundException('Aucun défi trouvé avec l\'id '.$id);
 		}
-	
+
 		if ($user != $challenge->getCreator() && !($this->get('security.context')->isGranted('ROLE_SUPER_ANIM'))) {
 			throw new AccessDeniedException();
 		}
-	
+
 		$chat = $this->get('doctrine_mongodb')
 			->getRepository('TheScienceTourMessageBundle:Chat')
 			->find($idchat);
-	
+
 		if (!$chat) {
 			throw $this->createNotFoundException('Aucune discussion trouvée avec l\'id '.$idchat);
 		}
 		if (!$challenge->getChats()->contains($chat)) {
 			throw $this->createNotFoundException('Cette discussion ne fait pas partie du défi');
 		}
-	
+
 		$dm = $this->get('doctrine_mongodb')->getManager();
 		foreach ($chat->getUsers() as $chatUser) {
 			$chatUser->removeNotification("chat", $chat->getId());
 			$dm->persist($chatUser);
 		}
 		$challenge->removeChat($chat);
-	
+
 		if (!($chat->getUsers()->count() == 1 && $chat->getUsers()->first() == $user)) {
-	
+
 			$from = $this->container->getParameter('mailer_sender_address');
 			$mail = \Swift_Message::newInstance()
 				->setSubject('The Science Tour : Discussion supprimée - défi '.$challenge->getTitle())
@@ -422,16 +446,16 @@ class ChallengeController extends Controller {
 					$mail->addBcc($chatUser->getEmail());
 				}
 			}
-				
+
 			$this->get('mailer')->send($mail);
-	
+
 		}
-	
+
 		$dm->remove($chat);
 		$dm->persist($challenge);
 		$dm->flush();
 		return $this->redirect($this->generateUrl('tst_challenge', array('id' => $challenge->getId(), 'tab' => "chats")));
-	
+
 	}
 
 }
