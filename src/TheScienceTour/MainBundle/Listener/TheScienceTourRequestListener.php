e9a85a00ab875f08b7859745379c28da0c02e539
Romain WEEGER <romain.weeger@gmail.com>
2016-11-29 10:13:48 +0100

    Detect language automatically

--- src/TheScienceTour/MainBundle/Listener/TheScienceTourRequestListener.php
+++ src/TheScienceTour/MainBundle/Listener/TheScienceTourRequestListener.php
@@ -4,6 +4,7 @@ namespace TheScienceTour\MainBundle\Listener;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  *
@@ -14,14 +15,33 @@ class TheScienceTourRequestListener {
   private $router;
   private $earsmusDomains;
 
-  public function __construct(Router $router, $earsmusDomains) {
+  public function __construct(Router $router, $earsmusDomains, $languages) {
     $this->router         = $router;
     $this->earsmusDomains = $earsmusDomains;
+    $this->languages      = $languages;
   }
 
   public function onKernelRequest(GetResponseEvent $event) {
     if (HttpKernelInterface::MASTER_REQUEST != $event->getRequestType()) {
       // don't do anything if it's not the master request
+      return;
+    }
+    // No lang specified on home.
+    if ($event->getRequest()->getPathInfo() == '/') {
+      // Search into given accepted languages.
+      $acceptedLanguages = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+      $langFound = FALSE;
+      foreach ($acceptedLanguages as $langPart) {
+        // Take only part before the ";"
+        $langPart = explode(';', $langPart)[0];
+        if (!$langFound && isset($this->languages[$langPart])) {
+          $langFound = $langPart;
+        }
+      }
+      // Build redirect response.
+      $event->setResponse(new RedirectResponse($event->getRequest()
+          ->getBaseUrl() . '/' . ($langFound ? $langFound : 'en')));
+      // No more work needed.
       return;
     }
     static $isErasmus = NULL;
