27e0ffd3cd4f18cf5d2049205bf9acb20e2819cd
root <root@sandbox3.cs805cloud.internal>
2017-08-17 02:01:53 +0200

    Ajout de docblocks

--- src/TheScienceTour/ChallengeBundle/Admin/ChallengeAdmin.php
+++ src/TheScienceTour/ChallengeBundle/Admin/ChallengeAdmin.php
@@ -9,14 +9,25 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Ivory\GoogleMap\Places\AutocompleteType;
 
+/**
+ * Classe concrète pour l'administration des objets du type "Challenge"
+ *
+ * Cette classe hérite de <strong>Sonata\AdminBundle\Admin\AbstractAdmin</strong> pour la gestion des objets Challenge
+ * danbs l'interface d'administration <em>ad hoc</em>
+ *
+ * @package TheScienceTour\ChallengeBundle\Admin
+ */
 class ChallengeAdmin extends Admin {
 
     /**
      * configureRoutes
      *
+     * Crée la liste des routes pour le menude l'interface d'administration
+     *
      * @param RouteCollection $collection
      */
-    protected function configureRoutes(RouteCollection $collection) {
+    protected function configureRoutes(RouteCollection $collection)
+    {
 		$collection
 			->remove('create')
 		;
@@ -27,7 +38,8 @@ class ChallengeAdmin extends Admin {
      *
      * @param FormMapper $formMapper
      */
-	protected function configureFormFields(FormMapper $formMapper) {
+	protected function configureFormFields(FormMapper $formMapper)
+    {
 		$formMapper
 			->add('title')
 			->add('creator')
@@ -43,6 +55,11 @@ class ChallengeAdmin extends Admin {
 
 	}
 
+    /**
+     * configureDatagridFilters
+     *
+     * @param DatagridMapper $datagridMapper
+     */
 	protected function configureDatagridFilters(DatagridMapper $datagridMapper)
 	{
 		$datagridMapper
@@ -51,7 +68,13 @@ class ChallengeAdmin extends Admin {
 		;
 	}
 
-	protected function configureListFields(ListMapper $listMapper) {
+    /**
+     * configureListFields
+     *
+     * @param ListMapper $listMapper
+     */
+	protected function configureListFields(ListMapper $listMapper)
+    {
 		$listMapper
 			->addIdentifier('title')
 			->add('creator', null, array('sortable' => false))
@@ -65,11 +88,23 @@ class ChallengeAdmin extends Admin {
 		;
 	}
 
-	public function preUpdate($project) {
+    /**
+     * preUpdate
+     *
+     * @param $project
+     */
+	public function preUpdate($project)
+    {
 		$project->updateFinishedAt();
 	}
 
-	public function getExportFields() {
+    /**
+     * getExportFields
+     *
+     * @return array
+     */
+	public function getExportFields() : array
+    {
 		return array(
 				'title',
 				'creator',
